b1d2425cd33f91c7d888bf9a8274ff0f2a968941
iFuradon <133047374+iFuradon@users.noreply.github.com>
2024-07-30 14:26:55 +0400

    init: Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+  let nStr = n.toString();
+  let reversedStr = nStr.split('').reverse().join('');
+  let resN = parseInt(reversedStr);
+  return resN; 
 }
